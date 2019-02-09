46e9af93952c5bbdbb40711d027b2f0a19da8410
pskwiercz <przemek.skwiercz@gmail.com>
2019-02-09 23:45:14 +0100

    Add Taco Design Controller

--- src/main/java/com/pskwiercz/tacocloud/controllers/HomeController.java
+++ src/main/java/com/pskwiercz/tacocloud/controllers/HomeController.java
@@ -10,9 +10,4 @@ public class HomeController {
     public String homePage() {
         return "home";
     }
-
-    @GetMapping("/home/")
-    public String homePg() {
-        return "home";
-    }
 }
