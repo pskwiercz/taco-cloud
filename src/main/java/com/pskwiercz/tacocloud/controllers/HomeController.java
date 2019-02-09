6995999e9c0d72a293e653428efe621aadd4c8f2
pskwiercz <przemek.skwiercz@gmail.com>
2019-02-09 20:16:17 +0100

    Add HomeController

---
+++ src/main/java/com/pskwiercz/tacocloud/controllers/HomeController.java
@@ -0,0 +1,18 @@
+package com.pskwiercz.tacocloud.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String homePage() {
+        return "home";
+    }
+
+    @GetMapping("/home/")
+    public String homePg() {
+        return "home";
+    }
+}
