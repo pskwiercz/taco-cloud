4013eacefc26525f00e0322839e4413d9e64ff96
pskwiercz <przemek.skwiercz@gmail.com>
2019-02-10 17:57:42 +0100

    Add View Controller

---
+++ src/main/java/com/pskwiercz/tacocloud/WebConfig.java
@@ -0,0 +1,16 @@
+package com.pskwiercz.tacocloud;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    // Can be use when we have few simple controllers which just make redirection
+    // Just add mapping path -> view name to registry and controller is not needed
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/home").setViewName("home");
+    }
+}
